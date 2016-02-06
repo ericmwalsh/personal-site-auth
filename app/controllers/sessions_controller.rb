0d712b99b63ad2e04901866fe5024acb27ff6889
Eric Walsh <walsh@spinlister.com>
2016-02-06 13:46:12 -0800

    configured logout for ember client

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,16 @@ class SessionsController < Devise::SessionsController
   end
 
   def destroy
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    unless token = request.headers['Auth-Token']
+      return head 403, reason: "Unauthorized user logout; no included authorization token."
+    end
+    if user = Admin.find_by_authentication_token(token)
+      sign_out user
+    else
+      render json: { error: "Logout failed; no user found." }, status: 403
+    end
+
+    # signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
     render json: { success: true }, status: 201
   rescue
     render json: { error: "Permission denied." }, status: 403
